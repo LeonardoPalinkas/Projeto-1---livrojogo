25845dade56d75eaa603783c5b4a3605882267ad
Leonardo Palinkas <palinkas@alunos.utfpr.edu.br>
2019-04-26 10:31:39 -0300

    Initial Commit

---
+++ Projeto 1 - livrojogo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Projeto 1 - livrojogo
+//
+//  Created by leonardo palinkas on 26/04/19.
+//  Copyright © 2019 leonardo palinkas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
