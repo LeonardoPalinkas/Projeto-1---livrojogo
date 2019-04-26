da82a3a46cfc1dde340bf6c5332664a392d73a00
Leonardo Palinkas <palinkas@alunos.utfpr.edu.br>
2019-04-26 13:18:52 -0300

    Esqueleto do Storyline funcional

---
+++ Projeto 1 - livrojogo/Examinar solo_ViewController.swift
@@ -0,0 +1,9 @@
+//
+//  Examinar solo.swift
+//  Projeto 1 - livrojogo
+//
+//  Created by leonardo palinkas on 26/04/19.
+//  Copyright © 2019 leonardo palinkas. All rights reserved.
+//
+
+import Foundation
---
+++ Projeto 1 - livrojogo/ViewControllers/InteragirAlienigenasViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InteragirAlienigenasViewController.swift
+//  Projeto 1 - livrojogo
+//
+//  Created by leonardo palinkas on 26/04/19.
+//  Copyright © 2019 leonardo palinkas. All rights reserved.
+//
+
+import UIKit
+
+class InteragirAlienigenasViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
