7b6dca47ab60bc397d5068102dfc3abe9b9cfc9e
Leonardo Palinkas <palinkas@alunos.utfpr.edu.br>
2019-04-28 21:55:20 -0300

    erro 1

--- Projeto 1 - livrojogo/AppDelegate.swift
+++ Projeto 1 - livrojogo/AppDelegate.swift
@@ -7,18 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var backGroundPlayers = AVAudioPlayer()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
-
+    
+    func playBackGroundMusic (fileNamed: String){
+        
+        let url = Bundle.main.url(forResource: fileNamed,
+                                  withExtension: nil)
+        guard let newUrl = url else{
+            
+            print("Could not find filed called \(fileNamed)")
+            return
+        }
+        do {
+            backGroundPlayers = try AVAudioPlayer(contentsOf: newUrl)
+            backGroundPlayers.numberOfLoops = -1
+            backGroundPlayers.prepareToPlay()
+            backGroundPlayers.play()
+        }
+        catch let error as NSError {
+            
+            print(error.description)
+        }
+        playBackGroundMusic(fileNamed: "Ether_Oar.mp3")
+    }
+    
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
